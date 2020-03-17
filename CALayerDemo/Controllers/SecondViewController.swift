3594ee8d6995f3efb97fad1adab15202bb49a448
DmitryYatsyuk-dv <yatsyuk.dev@mail.ru>
2020-03-17 19:45:04 +0300

    CAGradientLayer; CAShapeLayer; CABasicAnimation

--- CALayerDemo/Controllers/SecondViewController.swift
+++ CALayerDemo/Controllers/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  ThirdViewController.swift
 //  CALayerDemo
 //
 //  Created by Lucky on 16/03/2020.
@@ -8,37 +8,106 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var imageViewSecond: UIImageView!
+class SecondViewController: UIViewController, CAAnimationDelegate {
     
-    @IBOutlet weak var button: UIButton! {
+    var shapeLayer: CAShapeLayer!
+    var overShapeLayer: CAShapeLayer!
+    var borderLayer = CAShapeLayer()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var gradientLayer: CAGradientLayer! {
         didSet {
-            button.layer.cornerRadius = 15
-            button.layer.autoreverses = true
-            button.layer.shadowOffset = CGSize(width: 0, height: 0)
-            button.layer.shadowRadius = 10
-            button.layer.shadowOpacity = 0.6
-            button.layer.shadowColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+            
+            let startColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1).cgColor
+            let endColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1).cgColor
+            
+            gradientLayer.colors = [startColor, endColor] 
+//            gradientLayer.colors = [UIColor.red.cgColor, ... Other colors]
+            // Screen fill ratio
+//            gradientLayer.locations = [0, 0.20, 1]
         }
+    }
+
+    override func viewDidLayoutSubviews() {
+        gradientLayer.frame = CGRect(x: 0, y: 0,
+                                     width: self.view.bounds.size.width,
+                                     height: 20 + 271 + imageView.frame.size.height / 2)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        firstImageShapeLayer()
+        overImageShapeLayer()
         
-        
+        gradientLayer = CAGradientLayer()
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
+    @IBAction func Tapped(_ sender: UIButton) {
+    
+           let animation = CABasicAnimation(keyPath: "strokeEnd")
+               animation.toValue = 1
+               animation.duration = 4
+               animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+               animation.fillMode = CAMediaTimingFillMode.both
+               animation.isRemovedOnCompletion = false
+               animation.delegate = self
+               
+               borderLayer.add(animation, forKey: nil)
+           }
+       
+    func firstImageShapeLayer() {
+        // Add rounded corners
+        shapeLayer = CAShapeLayer()
+        
+        shapeLayer.frame = imageView.bounds
+        shapeLayer.path = UIBezierPath(roundedRect: imageView.bounds,
+                                       byRoundingCorners: .allCorners,
+                                       cornerRadii: CGSize(width: self.imageView.frame.width,
+                                                           height: self.imageView.frame.height)).cgPath
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        imageView.layer.mask = shapeLayer
+        
+        // Add border
+        borderLayer.path = shapeLayer.path
+        borderLayer.frame = imageView.bounds
+        borderLayer.lineWidth = 25
+        borderLayer.lineCap = CAShapeLayerLineCap(rawValue: "round")
+        borderLayer.fillColor = UIColor.clear.cgColor
+        borderLayer.strokeEnd = 1
+        let color = #colorLiteral(red: 0.7230684161, green: 0.8037459254, blue: 0.833204329, alpha: 1)
+        borderLayer.strokeColor = color.cgColor
+        imageView.layer.addSublayer(borderLayer)
     }
-    */
 
+    func overImageShapeLayer() {
+        
+        // Add rounded corners
+        overShapeLayer = CAShapeLayer()
+        
+        overShapeLayer.frame = imageView.bounds
+        overShapeLayer.path = UIBezierPath(roundedRect: imageView.bounds,
+                                           byRoundingCorners: .allCorners,
+                                           cornerRadii: CGSize(width: self.imageView.frame.width, height: self.imageView.frame.height)).cgPath
+        
+        imageView.layer.mask = overShapeLayer
+        
+        // Add border
+        borderLayer = CAShapeLayer()
+        borderLayer.path = overShapeLayer.path
+        borderLayer.frame = imageView.bounds
+        borderLayer.lineWidth = 25
+        borderLayer.lineCap = CAShapeLayerLineCap(rawValue: "round")
+        borderLayer.fillColor = nil
+        borderLayer.strokeEnd = 0
+        let color = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        borderLayer.strokeColor = color.cgColor
+        imageView.layer.addSublayer(borderLayer)
+    }
 }
--- CALayerDemo/Controllers/ViewController.swift
+++ CALayerDemo/Controllers/ViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CAAnimationDelegate {
 
+    var shapeLayer: CAShapeLayer! {
+        didSet {
+            shapeLayer.lineWidth = 20
+            shapeLayer.lineCap = CAShapeLayerLineCap(rawValue: "round")
+            shapeLayer.fillColor = nil
+            shapeLayer.strokeEnd = 1
+            let color = #colorLiteral(red: 0.4256846734, green: 0.5517628617, blue: 0.570057914, alpha: 1)
+            shapeLayer.strokeColor = color.cgColor
+        }
+    }
+    
+    var overShapeLayer: CAShapeLayer! {
+        didSet {
+            overShapeLayer.lineWidth = 20
+            overShapeLayer.lineCap = CAShapeLayerLineCap(rawValue: "round")
+            overShapeLayer.fillColor = nil
+            overShapeLayer.strokeEnd = 0
+            let color = #colorLiteral(red: 0.7230684161, green: 0.8037459254, blue: 0.833204329, alpha: 1)
+            overShapeLayer.strokeColor = color.cgColor
+        }
+    }
     
     @IBOutlet weak var imageView: UIImageView! {
         didSet {
@@ -28,10 +49,55 @@ class ViewController: UIViewController {
             button.layer.shadowColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1).cgColor
         }
     }
+
+    override func viewWillLayoutSubviews() {
+        
+        configShapeLayer(shapeLayer)
+        configShapeLayer(overShapeLayer)
+    }
+    
+    func configShapeLayer(_ shapeLayer: CAShapeLayer) {
+        
+        shapeLayer.frame = view.bounds
+        let path = UIBezierPath()
+        
+        path.move(to: CGPoint(x: self.view.frame.width / 2 - 100,
+                              y: self.view.frame.height / 1.52))
+        
+        path.addLine(to: CGPoint(x: self.view.frame.width / 2 + 100,
+                                 y: self.view.frame.height / 1.52))
+        shapeLayer.path = path.cgPath
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        shapeLayer = CAShapeLayer()
+        view.layer.addSublayer(shapeLayer)
+        
+        overShapeLayer = CAShapeLayer()
+        view.layer.addSublayer(overShapeLayer)
+    }
+    
+    @IBAction func questionTapped(_ sender: UIButton) {
+        
+//        overShapeLayer.strokeEnd += 0.2
+//        if overShapeLayer.strokeEnd == 1 {
+//            performSegue(withIdentifier: "showSecondScreen", sender: self)
+//        }
+        
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.toValue = 1
+        animation.duration = 2
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        animation.fillMode = CAMediaTimingFillMode.both
+        animation.isRemovedOnCompletion = false
+        animation.delegate = self
+        
+        overShapeLayer.add(animation, forKey: nil)
+    }
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        performSegue(withIdentifier: "showSecondScreen", sender: self)
     }
 }
 
